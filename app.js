e61a2fa4d14ce8e24febb84d2efaf71166d6464a
Prathmesh Kulkarni <kulkarniprathmesh1310@gmail.com>
2026-06-13 01:19:42 +0530

    final

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Testing jenkins 🚀");
+    res.send("Hi From Pratham 🚀");
 });
 
 app.listen(3000, () => {
-    console.log("Server running on port 3000");
+    console.log("Server running on port 3001");
 });
