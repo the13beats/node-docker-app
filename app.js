79b3e49b83a7b42c0eb43c9ffa2f813dd1e8cdb0
Prathmesh Kulkarni <kulkarniprathmesh1310@gmail.com>
2026-06-13 00:40:58 +0530

    new version

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Hello from Docker 🚀");
+    res.send("Version 2 deployed by jenkins 🚀");
 });
 
 app.listen(3000, () => {
