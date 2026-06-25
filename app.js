fccec864cc704a37d1b4aea3fd659fe5cbfdc93a
Prathmesh Kulkarni <kulkarniprathmesh1310@gmail.com>
2026-06-25 16:43:10 +0530

    new

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Hi From Pratham 🚀");
+    res.send("Hi From Pratham Kulkarni🚀");
 });
 
 app.listen(3000, () => {
